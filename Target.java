8858e1a09565ce487645f5913064e7b6c8a51d57
POKEstepheN <348921842@gapps.yrdsb.ca>
2022-12-19 07:05:36 -0500

    Added the target, bomb, and potion class

---
+++ Target.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The target is what the player is trying to shoot at.
+ * 
+ * @Stephen Liu 
+ * @December 19, 2022
+ */
+public class Target extends Actor
+{
+    /**
+     * Act - do whatever the Target wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
