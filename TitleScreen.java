0ea297ff268f2e6a89b53c5f2b33345a89f0b9a3
POKEstepheN <348921842@gapps.yrdsb.ca>
2022-12-19 09:24:51 -0500

    Add instructions to title screen

--- TitleScreen.java
+++ TitleScreen.java
@@ -15,14 +15,46 @@ public class TitleScreen extends World
     {    
         // Create a new world with 800x500 cells with a cell size of 1x1 pixels.
         super(800, 500, 1);
-        
+
         //Create and display a title
         Label title=new Label("Pokemon Targetry", 90);
         addObject(title, getWidth()/2, 100);
-        
+
         //Create and display instructions
+        Label continueGame=new Label("Press <space> to continue", 50);
+        addObject(continueGame, getWidth()/2, getHeight()/2);
+
+        Label directions=new Label("Press the arrow keys to move character", 30);
+        addObject(directions, getWidth()/2, 380);
+
+        Label objective1=new Label("Press <a> to shoot at ", 30);
+        addObject(objective1, getWidth()/2, 415);
+
+        Target titleTarget=new Target();
+        GreenfootImage targetImage=titleTarget.getImage();  
+        targetImage.scale(60, 60);
+        titleTarget.setImage(targetImage);
+        addObject(titleTarget, 540, 415);
+
+        Label objective2=new Label("Dodge the ", 30);
+        addObject(objective2, getWidth()/2, 450);
+        
+        Bomb titleBomb=new Bomb();
+        GreenfootImage bombImage=titleBomb.getImage();  
+        bombImage.scale(60, 60);
+        titleBomb.setImage(bombImage);
+        addObject(titleBomb, 478, 450);
+        
+        Label objective3=new Label("Touch potion to heal ", 30);
+        addObject(objective3, getWidth()/2, 485);
+        
+        Potion titlePotion=new Potion();
+        GreenfootImage potionImage=titlePotion.getImage();  
+        potionImage.scale(21, 30);
+        titlePotion.setImage(potionImage);
+        addObject(titlePotion, 525, 485);
     }
-    
+
     /**
      * Checks if the space bar is pressed and goes to the game if pressed.
      */
