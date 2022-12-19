8e5c95609b9df66269a2702af8e4401deb97b22f
POKEstepheN <348921842@gapps.yrdsb.ca>
2022-12-19 14:46:31 -0500

    Set size and add background for the main game

--- MyWorld.java
+++ MyWorld.java
@@ -9,12 +9,11 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class MyWorld extends World
 {
     /**
-     * Constructor for objects of class MyWorld.
-     * 
+     * Constructor for class MyWorld.
      */
     public MyWorld()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1);
+        // Create a new world with 800x500 cells with a cell size of 1x1 pixels.
+        super(800, 500, 1);
     }
 }
