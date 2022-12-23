cd8b2c66632ce654c38b87eb0df958f1bf8c9312
POKEstepheN <348921842@gapps.yrdsb.ca>
2022-12-22 22:38:04 -0500

    Add bomb animation

--- Bomb.java
+++ Bomb.java
@@ -7,11 +7,53 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @December 19, 2022
  */
 public class Bomb extends Actor{
+    //Creates a list to store the bomb's rotation animation
+    GreenfootImage[] rotationBomb=new GreenfootImage[4];
+    
+    //Sets the index and speed
+    int rotationBombIndex=0;
+    SimpleTimer rotationBombTimer=new SimpleTimer();
+    
     /**
-     * Act - do whatever the Bomb wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * Contructor for Bomb class
+     */
+    public Bomb(){
+        //The different rotation positions are created
+        for(int i=0; i<rotationBomb.length; i++){
+            rotationBomb[i]=new GreenfootImage("images/Bomb.png");
+            rotationBomb[i].rotate(-90*i);
+            rotationBomb[i].scale(77, 75);
+        }
+        
+        rotationBombTimer.mark();
+        setImage(rotationBomb[0]);
+    }
+    
+    /**
+     * This method displays different rotation images.
+     */
+    public void bombAnimate(){
+        if(rotationBombTimer.millisElapsed()<100){
+            return;
+        }
+        
+        //If 100 milliseconds have passed, the timer resets
+        rotationBombTimer.mark();
+        
+        //A new image is shown
+        setImage(rotationBomb[rotationBombIndex]);
+        rotationBombIndex=(rotationBombIndex+1)%rotationBomb.length;
+    }
+    
+    /**
+     * The bomb will perform actions like rotating and hitting the player
      */
     public void act(){
-        // Add your action code here.
+        /*
+         * If the bomb is in the main game world, it will animate.
+         */
+        if(getWorld().getClass()==MyWorld.class){
+            bombAnimate();
+        }
     }
 }
